66eed40fb9ac19251d4660f634e6db598004adce
Martin Kepplinger <martin.kepplinger@puri.sm>
2021-10-07 12:39:21 +0200

    media: imx: imx8mq-mipi-csi2: add more YUV formats
    to support the ov5640 and other similar sensors
    
    Signed-off-by: Martin Kepplinger <martin.kepplinger@puri.sm>

--- drivers/staging/media/imx/imx8mq-mipi-csi2.c
+++ drivers/staging/media/imx/imx8mq-mipi-csi2.c
@@ -202,10 +202,28 @@ static const struct csi2_pix_format imx8mq_mipi_csi_formats[] = {
 		.width = 14,
 	}, {
 	/* YUV formats */
+		.code = MEDIA_BUS_FMT_UYVY8_2X8,
+		.width = 16,
+	}, {
+		.code = MEDIA_BUS_FMT_VYUY8_2X8,
+		.width = 16,
+	}, {
 		.code = MEDIA_BUS_FMT_YUYV8_2X8,
 		.width = 16,
 	}, {
+		.code = MEDIA_BUS_FMT_YVYU8_2X8,
+		.width = 16,
+	}, {
+		.code = MEDIA_BUS_FMT_UYVY8_1X16,
+		.width = 16,
+	}, {
+		.code = MEDIA_BUS_FMT_VYUY8_1X16,
+		.width = 16,
+	}, {
 		.code = MEDIA_BUS_FMT_YUYV8_1X16,
+		.width = 16,
+	}, {
+		.code = MEDIA_BUS_FMT_YVYU8_1X16,
 		.width = 16,
 	}
 };
